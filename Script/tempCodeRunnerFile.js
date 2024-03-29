39bff9166c3ac091aa872a3ccece13e55603ac79
LingGrani <luthfiangger2510@gmail.com>
2024-03-29 21:39:57 +0700

    first commit

---
+++ Script/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+const DBKecil = 
+[
+  {Nama:"ayaka", Vision:"Cryo"},
+  {Nama:"ayato", Vision:"Hydro"}
+];
+
+console.log(DBKecil[0].Nama);
---
+++ Script/test.js
@@ -0,0 +1,7 @@
+var DBKecil = 
+[
+  {Nama:"ayaka", Vision:"Cryo"},
+  {Nama:"ayato", Vision:"Hydro"}
+];
+
+console.log(DBKecil[0].Nama);
